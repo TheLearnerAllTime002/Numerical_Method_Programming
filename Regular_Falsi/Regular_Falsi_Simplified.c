e0aedc4823ab87cd5d7293d479a707e417c5b06c
TheLearnerAllTime002 <arjunmitra06082004@gmail.com>
2025-02-28 10:08:41 +0530

    Updated formula for  Regular_Falsi

--- Regular_Falsi/Regular_Falsi_Simplified.c
+++ Regular_Falsi/Regular_Falsi_Simplified.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 #define EPSILON 0.0001
-#define f(x) (2 * (x) - 3 * sin(x) - 5) // Define your equation
+#define f(x) ((x) * (x) * (x) - 9 * (x) + 1) // write your own equation
 
 void find_root(float a, float b);
 
@@ -12,15 +12,15 @@ int main()
     printf("Enter the range (a b): ");
     scanf("%f %f", &a, &b);
 
-    float step = 0.1;
+    float step = 0.01; // searching frequency 
     int root_found = 0;
 
     for (float i = a; i < b; i += step)
     {
         if (i + step > b)
-            break; // Prevents out-of-range errors
+            break; 
 
-        if (f(i) * f(i + step) < 0) // Check for root existence
+        if (f(i) * f(i + step) < 0) 
         {
             printf("Root found in range [%.2f, %.2f]: ", i, i + step);
             find_root(i, i + step);
@@ -39,19 +39,24 @@ void find_root(float a, float b)
     float c, fc;
     int iteration = 0;
 
+    
     do
     {
-        c = (a * f(b) - b * f(a)) / (f(b) - f(a)); // Regula Falsi formula
-        fc = f(c);
-        iteration++;
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+        fc = f(c); 
+        iteration++; 
 
-        if (fabs(fc) < EPSILON) // Root found
+        if (fabs(fc) < EPSILON) 
             break;
 
-        (f(a) * fc < 0) ? (b = c) : (a = c); // Update interval
+        
+        if (f(a) * fc < 0)
+            b = c; 
+        else
+            a = c; 
 
-    } while (fabs(fc) > EPSILON);
+    } while (fabs(fc) > EPSILON); 
 
-    printf("%.6f\n", c);
+    
+    printf("Root: %.6f found after %d iterations\n", c, iteration);
 }
-
